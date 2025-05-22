4739972f5bd19cf91f7452e4154a21783d7ad990
Faizululum <faizululum25@gmail.com>
2025-05-22 11:57:24 +0700

    feat(nav): create navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
+import Image from "next/image"
 import styles from "./navbar.module.css"
+import Link from "next/link"
+import ThemeToggle from "../themeToggle/ThemeToggle"
+import AuthLink from "../authLink/AuthLink"
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      Navbar
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>Blog App</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Home</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/about">About</Link>
+        <AuthLink />
+      </div>
     </div>
   )
 }
