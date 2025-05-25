6a1d3537d62ceab7390304ee4640b79a5482f86b
Faizululum <faizululum25@gmail.com>
2025-05-25 20:28:48 +0700

    feat(home): add featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,11 +1,28 @@
-import styles from "./featured.module.css"
+import Image from "next/image";
+import styles from "./featured.module.css";
 
 const Featured = () => {
   return (
     <div className={styles.container}>
-      Featured
+      <h1 className={styles.title}>
+        <b>Hey, Faiz dev here!</b> Discover my stories and creative journey
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="post1" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ipsa
+            eius labore quo non? Quibusdam nisi ipsam cupiditate culpa ex, ipsum
+            esse, maiores voluptates veniam sit, accusamus tempore unde eos?
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
