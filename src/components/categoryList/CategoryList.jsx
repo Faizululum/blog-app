e9b2f69ca7549f99dba0bcce3bc2ba16b6a66485
Faizululum <faizululum25@gmail.com>
2025-05-26 09:50:58 +0700

    feat(cat): add category ui section

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,11 +1,93 @@
-import styles from "./categoryList.module.css"
+import Link from "next/link";
+import styles from "./categoryList.module.css";
+import Image from "next/image";
 
 const CategoryList = () => {
   return (
     <div className={styles.container}>
-      CategoryList
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/style.png"
+            alt="style"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          Style
+        </Link>
+        <Link
+          href="/blog?cat=fashion"
+          className={`${styles.category} ${styles.fashion}`}
+        >
+          <Image
+            src="/fashion.png"
+            alt="fashion"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          fashion
+        </Link>
+        <Link
+          href="/blog?cat=food"
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src="/food.png"
+            alt="food"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          food
+        </Link>
+        <Link
+          href="/blog?cat=travel"
+          className={`${styles.category} ${styles.travel}`}
+        >
+          <Image
+            src="/travel.png"
+            alt="travel"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          travel
+        </Link>
+        <Link
+          href="/blog?cat=culture"
+          className={`${styles.category} ${styles.culture}`}
+        >
+          <Image
+            src="/culture.png"
+            alt="culture"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          culture
+        </Link>
+        <Link
+          href="/blog?cat=coding"
+          className={`${styles.category} ${styles.coding}`}
+        >
+          <Image
+            src="/coding.png"
+            alt="coding"
+            width={24}
+            height={24}
+            className={styles.image}
+          />
+          coding
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
