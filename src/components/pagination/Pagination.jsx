04555588656029ba438b4f5d1813be65835cd989
Faizululum <faizululum25@gmail.com>
2025-05-26 14:32:18 +0700

    feat(btn): add pagination button

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -3,7 +3,8 @@ import styles from "./pagination.module.css"
 const Pagination = () => {
   return (
     <div className={styles.container}>
-      Pagination
+      <button className={styles.button}>Prev</button>
+      <button className={styles.button}>Next</button>
     </div>
   )
 }
