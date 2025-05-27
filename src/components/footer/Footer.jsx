ce5297db166a9f1f8e84defde25d472c8b63c2b2
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-05-27 09:53:00 +0700

    feat(ui): add footer component (#17)
    * feat(btn): add pagination button
    
    * feat(ui): add footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,53 @@
-import styles from "./footer.module.css"
+import Image from "next/image";
+import styles from "./footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      Footer
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>Faiz Blog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet porro
+          illum omnis ea quidem illo nesciunt corporis adipisci eligendi,
+          assumenda commodi quam cupiditate est eaque inventore libero dolorum
+          dolore quaerat.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+          <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+          <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
